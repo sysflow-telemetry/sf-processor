0c9ddacd195f2300289685722b9b84f98b661124
TPT <terylt@ibm.com>
2020-04-15 20:43:04 +0000

    Added example plugin based on a shared object for inclusion in the sysflow processor

---
+++ plugins/example/example.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	hdl "github.com/sysflow-telemetry/sf-apis/go/handlers"
+	sp "github.com/sysflow-telemetry/sf-apis/go/processors"
+	"sync"
+)
+
+type Example struct {
+}
+
+func NewExample() sp.SFProcessor {
+	return new(Example)
+}
+
+func (s *Example) Process(ch interface{}, wg *sync.WaitGroup) {
+	cha := ch.(*hdl.FlatChannel)
+	record := cha.In
+	fmt.Println("Example channel capacity:", cap(record))
+	defer wg.Done()
+	fmt.Println("Starting Example")
+	for {
+		fc, ok := <-record
+		if !ok {
+			fmt.Println("Channel closed. Shutting down.")
+			break
+		}
+		fmt.Println(fc)
+	}
+	fmt.Println("Exiting Example")
+}
+
+func (s *Example) SetOutChan(ch interface{}) {
+}
+
+func (s *Example) Cleanup() {
+}
---
+++ plugins/example/go.mod
@@ -0,0 +1,5 @@
+module github.ibm.com/sysflow/sf-processor/plugins/example
+
+go 1.14
+
+require github.com/sysflow-telemetry/sf-apis/go v0.0.0-20200415202402-e5659ec14bfd
