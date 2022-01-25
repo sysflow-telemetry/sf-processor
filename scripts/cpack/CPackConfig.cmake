9a049bb6e8840b8a126754ca9de46d4f4f73d9c1
Frederico Araujo <frederico.araujo@ibm.com>
2022-01-25 22:58:09 +0000

    refact(scripts): cleaned up commented lines from cpack script
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/cpack/CPackConfig.cmake
+++ scripts/cpack/CPackConfig.cmake
@@ -5,7 +5,6 @@ set(CPACK_PACKAGE_DESCRIPTION "SysFlow Processor implements a pluggable stream-p
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "SysFlow stream processing agent")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_LIST_DIR}/build/LICENSE.md")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_LIST_DIR}/build/README.md")
-#set(CPACK_PROJECT_CONFIG_FILE "${PROJECT_SOURCE_DIR}/cmake/cpack/CMakeCPackOptions.cmake")
 set(CPACK_STRIP_FILES "ON")
 set(CPACK_PACKAGE_RELOCATABLE "OFF")
 
@@ -41,16 +40,9 @@ if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
 endif()
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://github.com/sysflow-telemetry/sf-processor")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "dkms (>= 2.1.0.0)")
-#set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
-#    "${CMAKE_BINARY_DIR}/scripts/debian/postinst;${CMAKE_BINARY_DIR}/scripts/debian/prerm;${CMAKE_BINARY_DIR}/scripts/debian/postrm;${PROJECT_SOURCE_DIR}/cmake/cpack/debian/conffiles"
-#)
 set(CPACK_RPM_PACKAGE_LICENSE "Apache v2.0")
 set(CPACK_RPM_PACKAGE_ARCHITECTURE, "${CMAKE_SYSTEM_PROCESSOR}")
 set(CPACK_RPM_PACKAGE_URL "https://github.com/sysflow-telemetry/sf-processor")
-#set(CPACK_RPM_PACKAGE_REQUIRES "dkms, kernel-devel, systemd")
-#set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${CMAKE_BINARY_DIR}/scripts/rpm/postinstall")
-#set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${CMAKE_BINARY_DIR}/scripts/rpm/preuninstall")
-#set(CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE "${CMAKE_BINARY_DIR}/scripts/rpm/postuninstall")
 set(CPACK_RPM_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}")
 set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION
     /usr/src
@@ -63,12 +55,5 @@ set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION
 set(CPACK_RPM_PACKAGE_RELOCATABLE "OFF")
 
 set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_PROCESSOR})
-
-#execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_LIST_DIR}/build/usr/bin)
-#execute_process(COMMAND ln -s ${CPACK_PACKAGING_INSTALL_PREFIX}/bin/sfprocessor ${CMAKE_CURRENT_LIST_DIR}/build/usr/bin/sfprocessor)
-#execute_process(COMMAND mkdir -p ${CMAKE_CURRENT_LIST_DIR}/build/usr/bin; ln -s ${CPACK_PACKAGING_INSTALL_PREFIX}/bin/sfprocessor ${CMAKE_CURRENT_LIST_DIR}/build/usr/bin/sfprocessor)
 set(CPACK_INSTALLED_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/build/bin" "/usr/bin" "${CMAKE_CURRENT_LIST_DIR}/build/resources" "/etc/sysflow")
-
-#set(CPACK_INSTALL_COMMANDS "cp build/bin/sfprocessor.sl /usr/bin/sfprocessor")
-#install(FILES ${CMAKE_CURRENT_LIST_DIR}/sfprocessor DESTINATION /usr/bin/sfprocessor COMPONENT sfprocessor)
 
