7430ad5cc03db699b59660eaba8f0c93917ea952
Frederico Araujo <frederico.araujo@ibm.com>
2022-01-31 14:01:08 +0000

    build(scripts): added systemd service definition and sysflow launcher to packaged distributions
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/cpack/prepackage.sh
+++ scripts/cpack/prepackage.sh
@@ -3,6 +3,8 @@ set -e
 DIR=$(pwd)
 rm -rf build && mkdir -p build/bin build/resources
 cp ../../driver/sfprocessor $DIR/build/bin/.
+cp -r ../service/systemd $DIR/build/service
+cp ../service/bin/sysflow $DIR/build/bin/.
 cp -r ../../resources/pipelines $DIR/build/resources/.
 cp -r ../../resources/policies $DIR/build/resources/.
 cp -r ../../LICENSE.md $DIR/build/.
