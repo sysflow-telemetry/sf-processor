87c72dbaf1b9d6a85afd3fb5cfb61b06fd7a4da3
Frederico Araujo <frederico.araujo@ibm.com>
2022-01-31 14:01:08 +0000

    build(scripts): added systemd service definition and sysflow launcher to packaged distributions
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/cpack/CPackConfig.cmake
+++ scripts/cpack/CPackConfig.cmake
@@ -59,5 +59,5 @@ set(CPACK_RPM_PACKAGE_RELOCATABLE "OFF")
 
 # Contents
 set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_PROCESSOR})
-set(CPACK_INSTALLED_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/build/bin" "/usr/bin" "${CMAKE_CURRENT_LIST_DIR}/build/resources" "/etc/sysflow")
+set(CPACK_INSTALLED_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/build/bin" "/usr/bin" "${CMAKE_CURRENT_LIST_DIR}/build/resources" "/etc/sysflow" "${CMAKE_CURRENT_LIST_DIR}/build/service" "/usr/lib/systemd/system")
 
