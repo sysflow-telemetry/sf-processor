03a9d130aef03a27f16525da34b32ca0fec1a5a1
Frederico Araujo <frederico.araujo@ibm.com>
2022-02-04 22:29:33 +0000

    build(scripts): changed packaging to obtain binary from docker image
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/cpack/prepackage.sh
+++ scripts/cpack/prepackage.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
-DIR=$(pwd)
-rm -rf build && mkdir -p build/bin build/resources
-cp ../../driver/sfprocessor $DIR/build/bin/.
-cp -r ../service/systemd $DIR/build/service
-cp -r ../../resources/pipelines $DIR/build/resources/.
-cp -r ../../resources/policies $DIR/build/resources/.
-cp -r ../../LICENSE.md $DIR/build/.
-cp -r ../../README.md $DIR/build/.
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $DIR && rm -rf build && mkdir -p build
+cp -a $DIR/../../bin $DIR/build/bin
+cp -a $DIR/../service/systemd $DIR/build/service
+mkdir -p $DIR/build/resources
+cp -a $DIR/../../resources/pipelines $DIR/build/resources/.
+cp -a $DIR/../../resources/policies $DIR/build/resources/.
+cp $DIR/../../LICENSE.md $DIR/build/.
+cp $DIR/../../README.md $DIR/build/.
 cd $DIR
 
