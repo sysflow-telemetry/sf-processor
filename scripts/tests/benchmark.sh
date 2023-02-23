867a9de3643deec6253501fb4b0b27e672eb0984
Frederico Araujo <frederico.araujo@ibm.com>
2023-02-23 08:13:55 -0500

    feat(tests): new benchmark scripts
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/tests/benchmark.sh
+++ scripts/tests/benchmark.sh
@@ -9,11 +9,14 @@ fi
 DURATION=$1
 CONFIG=$2
 TRACES=$3
+OUTDIR=$4
+
+mkdir -p $OUTDIR
 
 for n in {1..10}
 do    
-    export POLICYENGINE_BENCH_RULESSAMPLESIZE=$(( $n*5 ))    
-    echo "Benchmarking with $POLICYENGINE_BENCH_RULESSAMPLESIZE rules"        
-    ( $tm $DURATION ../../driver/sfprocessor -perflog -log=quiet -config=$CONFIG -driver=file $TRACES > rate_$POLICYENGINE_BENCH_RULESSAMPLESIZE.out ) &
-    sleep $(( $DURATION + 30 ))
+    export POLICYENGINE_BENCH_RULESETSIZE=$(( $n*5 ))    
+    echo "Benchmarking with $POLICYENGINE_BENCH_RULESETSIZE rules"        
+    ( $tm $DURATION ../../driver/sfprocessor -perflog -log=quiet -config=$CONFIG -driver=file $TRACES > $OUTDIR/rate_$POLICYENGINE_BENCH_RULESETSIZE.out ) &
+    sleep 90
 done
