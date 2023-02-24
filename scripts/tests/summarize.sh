568a17d32fdc2c84ea9e403164cc5315f787963a
Frederico Araujo <frederico.araujo@ibm.com>
2023-02-24 14:53:50 +0000

    test(scripts): update summarize script
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- scripts/tests/summarize.sh
+++ scripts/tests/summarize.sh
@@ -10,12 +10,12 @@ DIR=${1:-"."}
 VALUES=""
 LABELS=""
 
-for n in {1..10}
-do    
-    export POLICYENGINE_BENCH_RULESSAMPLESIZE=$(( $n*5 ))
-    AVG=$(cat $DIR/rate_$POLICYENGINE_BENCH_RULESSAMPLESIZE.out | grep "Policy engine rate" | awk 'NR>2 {print $NF}' | awk '{ total += $1; count++ } END { print total/(1000*count) }')
+for f in `ls -v $DIR`
+do   
+    n=$(v=${f%.*} && printf "%s\n" "${v##*_}")
+    AVG=$(cat $DIR/$f | grep "Policy engine rate" | awk 'NR>2 {print $NF}' | awk '{ total += $1; count++ } END { print total/(1000*count) }')
     VALUES="$VALUES $AVG"
-    LABELS="$LABELS $POLICYENGINE_BENCH_RULESSAMPLESIZE"
+    LABELS="$LABELS $(( $n ))"
 done
 
 echo $VALUES
