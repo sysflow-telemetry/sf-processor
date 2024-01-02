eb269bea976bdfb3af4717e92924446e743ddfdb
Anthony Saieva <anthony.saieva@ibm.com>
2024-01-02 21:34:37 +0000

    chore: updating references to correct packages
    Signed-off-by: Anthony Saieva <anthony.saieva@ibm.com>

--- core/policyengine/source/otel/schema.go
+++ core/policyengine/source/otel/schema.go
@@ -1,26 +1,27 @@
 package otel
 
 import (
-	v1 "go.opentelemetry.io/proto/otlp/common/v1"
+	common "go.opentelemetry.io/proto/otlp/common/v1"
+	logs "go.opentelemetry.io/proto/otlp/logs/v1"
 )
 
 /* for now just wrapping around the OTEL types in
 case something more elaborate is needed */
 
-type ResourceLogs = v1.ResourceLogs
+type ResourceLogs = logs.ResourceLogs
 
-type KeyValue = v1.KeyValue
+type KeyValue = common.KeyValue
 
-type StringValue = v1.AnyValue_StringValue
+type StringValue = common.AnyValue_StringValue
 
-type ArrayValue = v1.AnyValue_ArrayValue
+type ArrayValue = common.AnyValue_ArrayValue
 
-type BoolValue = v1.AnyValue_BoolValue
+type BoolValue = common.AnyValue_BoolValue
 
-type BytesValue = v1.AnyValue_BytesValue
+type BytesValue = common.AnyValue_BytesValue
 
-type DoubleValue = v1.AnyValue_DoubleValue
+type DoubleValue = common.AnyValue_DoubleValue
 
-type IntValue = v1.AnyValue_IntValue
+type IntValue = common.AnyValue_IntValue
 
-type KvListValue = v1.AnyValue_KvlistValue
+type KvListValue = common.AnyValue_KvlistValue
