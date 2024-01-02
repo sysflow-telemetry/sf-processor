5814970ad63e77c96ad5ba2c5471cc607b9368b0
Anthony Saieva <anthony.saieva@ibm.com>
2024-01-02 21:33:13 +0000

    feat: first pass at otel policy engine adapter (broken)
    Signed-off-by: Anthony Saieva <anthony.saieva@ibm.com>

---
+++ core/policyengine/policyengine_otel.go
@@ -0,0 +1,17 @@
+//go: build otel
+//go:build otel
+// +build otel
+
+package policyengine
+
+import (
+	logs "go.opentelemetry.io/proto/otlp/logs/v1"
+)
+
+func (s *PolicyEngine) bypassPolicyEngine(rec *logs.ResourceLogs) {
+	s.out(rec)
+}
+
+func (s *PolicyEngine) processAsync(rec *logs.ResourceLogs) {
+	s.pi.ProcessAsync(rec)
+}
