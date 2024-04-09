1d8c8131c9f2c0b025e6698d4f8365428b4afda4
Anthony Saieva <anthony.saieva@ibm.com>
2024-04-09 19:50:41 +0000

    feat: adding kafka support to otelexporter
    Signed-off-by: Anthony Saieva <anthony.saieva@ibm.com>

--- core/processor/otelexporter.go
+++ core/processor/otelexporter.go
@@ -1,12 +1,15 @@
 package processor
 
 import (
+	"encoding/json"
 	"fmt"
 	"sync"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/sysflow-telemetry/sf-apis/go/logger"
 	"github.com/sysflow-telemetry/sf-apis/go/plugins"
 	"github.com/sysflow-telemetry/sf-processor/core/policyengine/source/otel"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -15,7 +18,11 @@ const (
 
 var Plugin OTELExporter
 
-type OTELExporter struct{}
+type OTELExporter struct {
+	producer    *kafka.Producer
+	exportTopic string
+	encoding    string
+}
 
 func NewOTELExporter() plugins.SFProcessor {
 	return new(OTELExporter)
@@ -26,6 +33,43 @@ func (s *OTELExporter) GetName() string {
 }
 
 func (s *OTELExporter) Init(conf map[string]interface{}) error {
+	brokerString, ok := conf["otelExportKafkaBrokerList"]
+	if !ok {
+		return fmt.Errorf("no broker list found to initialize driver")
+	}
+
+	topicRaw, ok := conf["otelExportTopic"]
+	if !ok {
+		return fmt.Errorf("no topic to export to")
+	}
+	topicStr, ok := topicRaw.(string)
+	if !ok {
+		return fmt.Errorf("invalid export topic")
+	}
+
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": brokerString,
+		"client.id":         "sfprocessor-otel-kafka-exporter",
+		"acks":              "all",
+	})
+
+	if err != nil {
+		return fmt.Errorf("invalid exporter config -- could not make producer")
+	}
+
+	enc, ok := conf["encoding"]
+	if !ok {
+		return fmt.Errorf("invalid config -- no encoding")
+	}
+	encStr, ok := enc.(string)
+	if (enc != "proto" && enc != "json") || !ok {
+		return fmt.Errorf("invalid config -- (%s) encoding not supported", enc)
+	}
+	s.encoding = encStr
+
+	s.producer = producer
+	s.exportTopic = topicStr
+
 	return nil
 }
 
@@ -45,10 +89,34 @@ func (s *OTELExporter) Process(ch []interface{}, wg *sync.WaitGroup) {
 				logger.Trace.Println("Channel closed shutting down")
 				break
 			}
-			fmt.Printf("Dealing with a record--%s\n", fc)
+			// fmt.Printf("Dealing with a record--%s\n", fc)
+
+			var msgValue []byte
+			var err error
+			if s.encoding == "json" {
+				msgValue, err = json.Marshal(fc)
+				if err != nil {
+					logger.Trace.Println("failed to serialize record to json")
+				}
+			} else if s.encoding == "proto" {
+				msgValue, err = proto.Marshal(fc)
+				if err != nil {
+					logger.Trace.Println("failed to serialize to record proto")
+				}
+			}
+
+			err = s.producer.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{
+					Topic:     &s.exportTopic,
+					Partition: kafka.PartitionAny,
+				},
+				Value: msgValue,
+			},
+				nil,
+			)
+
 		}
 	}
-	logger.Trace.Println("Exiting otel exporter")
 }
 
 func (s *OTELExporter) SetOutChan(ch []interface{}) {}
